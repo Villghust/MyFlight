b80ba5176b2344989adadff6a4c336205cbe7666
Marcelo Kohut <marcelo.kohut@gmail.com>
2018-06-13 00:53:22 -0300

    GerenciadorRotas implementado metodo para leitura de arquivo.

--- src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,19 +1,52 @@
 package pucrs.myflight.modelo;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class GerenciadorRotas {
-
     private ArrayList<Rota> rotas;
 
     public GerenciadorRotas() {
         this.rotas = new ArrayList<>();
     }
 
-    public void ordenarCias() {
-        Collections.sort(rotas);
+
+    public void carregaRotas(GerenciadorCias gerenciadorCias, GerenciadorAeroportos GerenciadorAeroportos, GerenciadorAeronaves GerenciadorAeronaves) throws IOException {
+        Path path2 = Paths.get("routes.dat");
+        try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.forName("utf8")))) {
+            sc.useDelimiter("[;\n ]");
+            String header = sc.nextLine();
+            String cia, origem, destino, aeronave;
+
+            while (sc.hasNext()) {
+                cia = sc.next();
+                origem = sc.next();
+                destino = sc.next();
+                aeronave = sc.next();
+
+                CiaAerea cia1 = gerenciadorCias.buscarCodigo(cia);
+                Aeroporto origem1 = GerenciadorAeroportos.buscarCodigo(origem);
+                Aeroporto destino1 = GerenciadorAeroportos.buscarCodigo(destino);
+                Aeronave aeronave1 = GerenciadorAeronaves.buscarCodigo(aeronave);
+                Rota rota = new Rota(cia1, origem1, destino1, aeronave1);
+                adicionar(rota);
+            }
+
+        }
     }
+
+
+
+        public void ordenarCias(){
+            Collections.sort(rotas);
+        }
+
 
     public void ordenarNomesCias() {
         rotas.sort( (Rota r1, Rota r2) ->
