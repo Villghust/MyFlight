7840db3833ba8ca6e023d8885896b6cd4188c697
Marcelo Kohut <marcelo.kohut@gmail.com>
2018-06-13 00:40:49 -0300

    GerenciadorAeroporto modificada para map e implementado metodo para leitura de arquivo.

--- src/pucrs/myflight/modelo/GerenciadorAeroportos.java
+++ src/pucrs/myflight/modelo/GerenciadorAeroportos.java
@@ -1,32 +1,62 @@
 package pucrs.myflight.modelo;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class GerenciadorAeroportos {
+    private Map<String, Aeroporto> aeroportos;
 
-    private ArrayList<Aeroporto> aeroportos;
-
-    public GerenciadorAeroportos() {
-        this.aeroportos = new ArrayList<>();
+    public GerenciadorAeroportos(){
+        this.aeroportos = new LinkedHashMap<>();
     }
 
-    public void ordenarNomes() {
-        Collections.sort(aeroportos);
+    public void carregaDados(String nomeArq) throws IOException {
+        Path path = Paths.get(nomeArq);
+        try (Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.forName("utf8")))) {
+            sc.useDelimiter("[;\n]");
+            String header = sc.nextLine();
+            String cod, nome, latitude, longitude;
+
+            //  String  loc; dividido em lat e long;
+
+            while (sc.hasNext()) {
+                cod = sc.next();
+                nome = sc.next();
+                latitude = sc.next();
+                longitude = sc.next();
+
+                //loc = sc.nextLine();  não da para colocar direto, pois é um objeto
+
+                Geo geo = new Geo(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                Aeroporto nova = new Aeroporto(cod, nome, geo); // <- Criar classe pais e adicionar um nova variavel aqui
+                adicionar(nova);
+
+            }
+        }
     }
+
+//    public void ordenarNomes() {
+//        Collections.sort(aeroportos);
+//    }
 
     public void adicionar(Aeroporto aero) {
-        aeroportos.add(aero);
+        aeroportos.put(aero.getCodigo(),
+                aero);
     }
 
     public ArrayList<Aeroporto> listarTodos() {
-        return new ArrayList<>(aeroportos);
+        return new ArrayList<>(aeroportos.values());
     }
 
     public Aeroporto buscarCodigo(String codigo) {
-        for(Aeroporto a: aeroportos)
-            if(a.getCodigo().equals(codigo))
-                return a;
-        return null;
+        return aeroportos.get(codigo);
+//        for(Aeroporto a: aeroportos)
+//            if(a.getCodigo().equals(codigo))
+//                return a;
+//        return null;
     }
 }
